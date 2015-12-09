45519a5c3ccf82da910f416af90a844adaec0ef6
fault <critical@fault.io>
2015-12-08 22:32:02 -0700

    Add index extraction for dumping a full scan of keys-values.

---
+++ bin/index.py
@@ -0,0 +1,37 @@
+"""
+Emit a newline separated list of keys contained in the hash to standard output.
+"""
+import itertools
+from .. import library
+from ...routes import library as libroutes
+
+def main(output, args, str=str):
+	write = output.write
+	directory, *typ = args
+	directory = libroutes.File.from_path(directory).fullpath
+	d = library.Dictionary.open(directory)
+
+	if typ and typ[0] == 'xml':
+		# Expose this outside of the binary.
+		from ...xml import library as libxml
+		content = libxml.element('map',
+			itertools.chain.from_iterable(
+				libxml.element('item',
+					libxml.escape_element_string(str(r)),
+					('key', k.decode('utf-8'))
+				)
+				for k, r in d.references()
+			),
+			('dictionary', directory),
+			('xmlns', 'https://fault.io/xml/filesystem#index'),
+		)
+		sep = b''
+	else:
+		content = d.keys()
+		sep = b'\n'
+
+	write(sep.join(content))
+
+if __name__ == '__main__':
+	import sys
+	main(sys.stdout.buffer, sys.argv[1:])

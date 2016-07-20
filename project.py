13ae95cb126c7f69d1c6261b4f6d404b686d879b
fault <critical@fault.io>
2016-07-20 09:38:05 -0700

    Update project information for consistency

--- project.py
+++ project.py
@@ -1,29 +1,9 @@
-abstract = 'storage abstractions for event streams and sub-filesystems'
-
-#: Project name.
+identity = 'https://fault.io/project/python/fault.filesystem'
 name = 'filesystem'
-
-#: The name of the conceptual branch of development.
-fork = 'lattice' # Explicit branch name and a codename for the major version of the project.
-release = None # A number indicating its position in the releases of a branch. (fork)
-
-#: The particular study or subject that the package is related to.
-study = {}
-
-#: Relevant emoji or reference--URL or relative file path--to an image file.
+abstract = 'Storage abstractions for managing filesystem based dictionaries and streams.'
 icon = '💾'
 
-#: IRI based project identity. (project homepage)
-identity = 'https://fault.io/project/python/filesystem'
+fork = 'lattice' # Explicit branch name and a codename for the major version of the project.
 
-#: Responsible Party
 controller = 'fault.io'
-
-#: Contact point for the Responsible Party
 contact = 'mailto:critical@fault.io'
-
-#: Version tuple: (major, minor, patch)
-version_info = (0, 1, 0)
-
-#: The version string.
-version = '.'.join(map(str, version_info))

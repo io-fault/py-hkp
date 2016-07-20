0e1573c7571573098a5752cf29a658dcdca53acf
fault <critical@fault.io>
2016-07-20 12:41:57 -0700

    Imply period.

--- project.py
+++ project.py
@@ -1,6 +1,6 @@
 identity = 'https://fault.io/project/python/fault.filesystem'
 name = 'filesystem'
-abstract = 'Storage abstractions for managing filesystem based dictionaries and streams.'
+abstract = 'Storage abstractions for managing filesystem based dictionaries and streams'
 icon = '💾'
 
 fork = 'lattice' # Explicit branch name and a codename for the major version of the project.

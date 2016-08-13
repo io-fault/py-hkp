9a00872257f9a41033950f3d12de0321962903f7
fault <critical@fault.io>
2016-08-13 08:45:46 -0700

    Add status field.

--- project.py
+++ project.py
@@ -4,6 +4,7 @@ abstract = 'Storage abstractions for managing filesystem based dictionaries and 
 icon = '💾'
 
 fork = 'lattice' # Explicit branch name and a codename for the major version of the project.
+status = 'flux'
 
 controller = 'fault.io'
 contact = 'mailto:critical@fault.io'

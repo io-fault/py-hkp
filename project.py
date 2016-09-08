003606a2dd83fe51345d40625bb2e935d7e4ba7f
fault <critical@fault.io>
2016-09-08 10:56:11 -0700

    Use unqualified names

--- project.py
+++ project.py
@@ -1,9 +1,10 @@
-identity = 'http://fault.io/python/fault.filesystem'
+identity = 'http://fault.io/python/filesystem'
 name = 'filesystem'
 abstract = 'Storage abstractions for managing filesystem based dictionaries and streams'
 icon = '💾'
 
 fork = 'lattice' # Explicit branch name and a codename for the major version of the project.
+versioning = 'continuous'
 status = 'flux'
 
 controller = 'fault.io'
